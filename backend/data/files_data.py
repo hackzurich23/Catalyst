deff6f539ecc74763359e256b9cfe978f68cf378
ArnavJPortfolio <arnav0422@berkeley.edu>
2023-09-16 08:11:40 -0700

    Added Data Files

--- backend/data/files_data.py
+++ backend/data/files_data.py
@@ -15,4 +15,28 @@ ALL_Files = [
         product="3D CONCRETE PRINTING",
         department="Construction",
     ),
+    FileData(
+        file_path="data/files_data/Waterproofing.txt",
+        file_link="https://usa.sika.com/en/construction/floor-covering/tile-stone-installation/waterproofing.html",
+        product="Waterproofing",
+        department="Construction",
+    ),
+    FileData(
+        file_path="data/files_data/Grout (Tiles).txt",
+        file_link="https://usa.sika.com/en/construction/floor-covering/tile-stone-installation/grouts.html",
+        product="Grout",
+        department="Construction",
+    ),
+    FileData(
+        file_path="data/files_data/Anchor Fix Adhesive.txt",
+        file_link="https://usa.sika.com/content/dam/dms/us01/t/sika_anchorfix_-1.pdf",
+        product="Grout",
+        department="Adhesives",
+    ),
+    FileData(
+        file_path="data/files_data/CarboDur Rods",
+        file_link="https://usa.sika.com/content/dam/dms/us01/b/sika_carbodur_rods.pdf",
+        product="CarboDur Rods",
+        department="Construction",
+    ),
 ]
